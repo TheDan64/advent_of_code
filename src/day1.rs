bae739ef205dd5a62df4104f480841ab184876ed
Daniel Kolsoi <ThaDan64@gmail.com>
2020-12-01 19:50:59 -0500

    Added input generator to day1 for slightly more accurate benchmarks

--- src/day1.rs
+++ src/day1.rs
@@ -1,14 +1,17 @@
-#[aoc(day1, part1)]
-pub fn part1_chars(input: &str) -> u32 {
-    let num_strs: Vec<_> = input
+#[aoc_generator(day1)]
+pub fn input_generator(input: &str) -> Vec<u32> {
+    input
         .split('\n')
         .filter(|s| !s.is_empty())
-        .map(|s| s.parse::<u32>().unwrap())
-        .collect();
+        .map(|s| s.parse().unwrap())
+        .collect()
+}
 
+#[aoc(day1, part1)]
+pub fn part1(nums: &[u32]) -> u32 {
     // Optimized solution
-    for (i, num) in num_strs.iter().enumerate() {
-        for num2 in &num_strs[i + 1..] {
+    for (i, num) in nums.iter().enumerate() {
+        for num2 in &nums[i + 1..] {
             if num + num2 == 2020 {
                 return num * num2;
             }
@@ -18,18 +21,12 @@ pub fn part1_chars(input: &str) -> u32 {
     unreachable!()
 }
 
-#[aoc(day1, part2, Chars)]
-pub fn part2_chars(input: &str) -> u32 {
-    let num_strs: Vec<_> = input
-        .split('\n')
-        .filter(|s| !s.is_empty())
-        .map(|s| s.parse::<u32>().unwrap())
-        .collect();
-
-    // More performant than before, but still naive solution
-    for (i, num) in num_strs.iter().enumerate() {
-        for (j, num2) in num_strs[i + 1..].iter().enumerate() {
-            for num3 in &num_strs[i + j + 2..] {
+#[aoc(day1, part2)]
+pub fn part2(nums: &[u32]) -> u32 {
+    // Optimized solution
+    for (i, num) in nums.iter().enumerate() {
+        for (j, num2) in nums[i + 1..].iter().enumerate() {
+            for num3 in &nums[i + j + 2..] {
                 if num + num2 + num3 == 2020 {
                     return num * num2 * num3;
                 }
