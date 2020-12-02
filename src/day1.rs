9cdd45a7f4826965fffb52309ca8ca4bef264771
Daniel Kolsoi <ThaDan64@gmail.com>
2020-12-01 20:10:12 -0500

    Update text

--- src/day1.rs
+++ src/day1.rs
@@ -9,7 +9,7 @@ pub fn input_generator(input: &str) -> Vec<u32> {
 
 #[aoc(day1, part1)]
 pub fn part1(nums: &[u32]) -> u32 {
-    // Optimized solution
+    // Slightly less naive solution
     for (i, num) in nums.iter().enumerate() {
         for num2 in &nums[i + 1..] {
             if num + num2 == 2020 {
@@ -23,7 +23,7 @@ pub fn part1(nums: &[u32]) -> u32 {
 
 #[aoc(day1, part2)]
 pub fn part2(nums: &[u32]) -> u32 {
-    // Optimized solution
+    // Slightly less naive solution
     for (i, num) in nums.iter().enumerate() {
         for (j, num2) in nums[i + 1..].iter().enumerate() {
             for num3 in &nums[i + j + 2..] {
