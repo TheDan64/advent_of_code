d4d0217f7ab325e0beefc74308b959b629217726
Daniel Kolsoi <ThaDan64@gmail.com>
2020-12-01 20:33:12 -0500

    Optimize d1p1

--- src/day1.rs
+++ src/day1.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 #[aoc_generator(day1)]
 pub fn input_generator(input: &str) -> Vec<u32> {
     input
@@ -9,13 +11,15 @@ pub fn input_generator(input: &str) -> Vec<u32> {
 
 #[aoc(day1, part1)]
 pub fn part1(nums: &[u32]) -> u32 {
-    // Slightly less naive solution
-    for (i, num) in nums.iter().enumerate() {
-        for num2 in &nums[i + 1..] {
-            if num + num2 == 2020 {
-                return num * num2;
-            }
+    let mut map = HashSet::with_capacity(nums.len());
+
+    // Optimized solution: O(n) worst case
+    for num in nums {
+        if map.get(&(2020 - num)).is_some() {
+            return num * (2020 - num);
         }
+
+        map.insert(*num);
     }
 
     unreachable!()
@@ -23,7 +27,7 @@ pub fn part1(nums: &[u32]) -> u32 {
 
 #[aoc(day1, part2)]
 pub fn part2(nums: &[u32]) -> u32 {
-    // Slightly less naive solution
+    // Slightly less naive solution: O(𝑛^3 − 3𝑛^2 + 2𝑛)
     for (i, num) in nums.iter().enumerate() {
         for (j, num2) in nums[i + 1..].iter().enumerate() {
             for num3 in &nums[i + j + 2..] {
