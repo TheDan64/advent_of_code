27f3376949637aa1ad2f57d895f7411a4b726dbd
Daniel Kolsoi <ThaDan64@gmail.com>
2020-12-01 19:24:23 -0500

    Added naive solutions for day1

--- src/day1.rs
+++ src/day1.rs
@@ -1,37 +1,39 @@
-fn fuel_req(num: i32) -> i32 {
-    num / 3 - 2
-}
+#[aoc(day1, part1)]
+pub fn part1_chars(input: &str) -> u32 {
+    let num_strs = input
+        .split('\n')
+        .filter(|s| !s.is_empty())
+        .map(|s| s.parse::<u32>().unwrap());
 
-#[aoc(day1, part1, Chars)]
-pub fn part1_chars(input: &str) -> i32 {
-    let mut sum = 0;
-    let num_strs = input.split('\n').filter(|s| !s.is_empty());
-
-    for num_str in num_strs {
-        sum += fuel_req(num_str.parse().unwrap());
-    }
-
-    sum
-}
-
-#[aoc(day1, part2, Chars)]
-pub fn part2_chars(input: &str) -> i32 {
-    let mut sum = 0;
-    let num_strs = input.split('\n').filter(|s| !s.is_empty());
-
-    for num_str in num_strs {
-        let mut req = num_str.parse().unwrap();
-
-        loop {
-            req = fuel_req(req);
-
-            if req <= 0 {
-                break;
+    // Naive solution; yolo
+    for num in num_strs.clone() {
+        for num2 in num_strs.clone() {
+            if num + num2 == 2020 {
+                return num * num2;
             }
-
-            sum += req;
         }
     }
 
-    sum
+    unreachable!()
+}
+
+#[aoc(day1, part2, Chars)]
+pub fn part2_chars(input: &str) -> u32 {
+    let num_strs = input
+        .split('\n')
+        .filter(|s| !s.is_empty())
+        .map(|s| s.parse::<u32>().unwrap());
+
+    // Naive solution; yolo
+    for num in num_strs.clone() {
+        for num2 in num_strs.clone() {
+            for num3 in num_strs.clone() {
+                if num + num2 + num3 == 2020 {
+                    return num * num2 * num3;
+                }
+            }
+        }
+    }
+
+    unreachable!()
 }
