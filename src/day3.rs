7b3878a6aa2ca28950f04bcd1437239cf2041e44
Daniel Kolsoi <ThaDan64@gmail.com>
2025-12-04 00:44:07 -0500

    Day 3

--- src/day3.rs
+++ src/day3.rs
@@ -1,49 +1,92 @@
-use std::collections::HashSet;
-use std::iter::FromIterator;
-
 use aoc_runner_derive::aoc;
-use itertools::Itertools;
-
-fn compartments<'s>(input: &'s str) -> impl Iterator<Item = (HashSet<char>, HashSet<char>)> + 's {
-    input.split('\n').map(|rucksack| {
-        let (left, right) = rucksack.split_at(rucksack.len() / 2);
-        (
-            HashSet::from_iter(left.chars()),
-            HashSet::from_iter(right.chars()),
-        )
-    })
-}
-
-fn score(ch: &char) -> u64 {
-    match ch {
-        'a'..='z' => *ch as u64 - 96,
-        'A'..='Z' => *ch as u64 - 38,
-        _ => unreachable!(),
-    }
-}
 
 #[aoc(day3, part1)]
-pub fn part1(input: &str) -> u64 {
-    compartments(input)
-        .map(|(left, right)| left.intersection(&right).map(score).sum::<u64>())
-        .sum()
+pub fn part1(input: &str) -> u32 {
+    let banks = input.lines();
+    let mut joltage = 0;
+
+    for bank in banks {
+        let mut greatest_pos = 0usize;
+        let mut greatest = 0;
+
+        for (pos, ch) in bank.chars().take(bank.len() - 1).enumerate() {
+            let digit = ch.to_digit(10).unwrap();
+
+            if digit > greatest {
+                greatest_pos = pos;
+                greatest = digit;
+            }
+        }
+
+        let next_greatest = bank.chars().skip(greatest_pos + 1).max().unwrap();
+        let next_greatest = next_greatest.to_digit(10).unwrap();
+
+        joltage += greatest * 10 + next_greatest;
+    }
+
+    joltage
+}
+
+fn prune(batteries: &mut Vec<char>) -> String {
+    loop {
+        let len = batteries.len();
+
+        if len == 12 {
+            break;
+        }
+
+        let mut iter = batteries.iter_mut().enumerate().peekable();
+
+        while let Some((pos, ch)) = iter.next() {
+            let digit = ch.to_digit(10).unwrap();
+            let Some((_, next_digit)) = iter.peek() else {
+                batteries.pop();
+                break;
+            };
+
+            let next_digit = next_digit.to_digit(10).unwrap();
+
+            if digit < next_digit {
+                batteries.remove(pos);
+                break;
+            }
+        }
+    }
+
+    batteries.iter().copied().collect()
 }
 
 #[aoc(day3, part2)]
 pub fn part2(input: &str) -> u64 {
-    compartments(input)
-        .chunks(3)
-        .into_iter()
-        .map(|group| {
-            let mut sets = group.map(|(s1, s2)| &s1 | &s2);
-            sets.next()
-                .map(|set| {
-                    sets.fold(set, |set1, set2| &set1 & &set2)
-                        .iter()
-                        .map(score)
-                        .sum::<u64>()
-                })
-                .unwrap()
-        })
-        .sum()
+    let banks = input.lines();
+    let mut joltage = 0u64;
+
+    for bank in banks {
+        let mut batteries = bank.chars().collect::<Vec<_>>();
+        let pruned = prune(&mut batteries);
+
+        joltage += pruned.parse::<u64>().unwrap();
+    }
+
+    joltage
+}
+
+#[test]
+fn test_day3_prune() {
+    assert_eq!(
+        prune(&mut "987654321111111".chars().collect()),
+        "987654321111"
+    );
+    assert_eq!(
+        prune(&mut "811111111111119".chars().collect()),
+        "811111111119"
+    );
+    assert_eq!(
+        prune(&mut "818181911112111".chars().collect()),
+        "888911112111"
+    );
+    assert_eq!(
+        prune(&mut "234234234234278".chars().collect()),
+        "434234234278"
+    );
 }
